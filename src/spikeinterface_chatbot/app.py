35b9b12668fb99b41e704d94da8807a45c3f2f63
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-03-23 09:41:05 +0100

    added both answer and question in deque

--- src/spikeinterface_chatbot/app.py
+++ src/spikeinterface_chatbot/app.py
@@ -1,5 +1,4 @@
-import os
-
+from collections import deque
 from flask import Flask, request, render_template
 
 from spikeinterface_chatbot.lang_chain_machinery import query_documentation
@@ -10,12 +9,15 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def chat():
     if request.method == "POST":
-        chat_history = [] if not locals().get("chat_history") else chat_history  # Hack to initalize, TODO: Improve
-        message = request.form["message"]
+        chat_history = (
+            deque(maxlen=3) if not locals().get("chat_history") else chat_history
+        )  # Hack to initalize, TODO: Improve
+        question = request.form["message"]
         persist_directory = "./data/chroma_database"
-        response = query_documentation(query=message, chat_history=chat_history, persist_directory=persist_directory)
-        chat_history.append(message)
-        return response
+        anwser = query_documentation(query=question, chat_history=chat_history, persist_directory=persist_directory)
+        chat_history.appendleft((question, anwser))
+
+        return anwser
     else:
         return render_template("chat.html")
 
